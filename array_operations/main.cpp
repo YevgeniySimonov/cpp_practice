2907b7909c1c738ed3a32252c87e21d3c4e60661
Yevgeniy Simonov <yevgeniy.simonov@theugroup.co>
2022-03-30 00:13:56 +0800

    feat: matrix multiplication

--- array_operations/main.cpp
+++ array_operations/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+// #include </omp.h> // TODO
 
 #define M 4
 #define N 5
 #define P 3
+#define THREAD_NUM 4
+
+// compillation
+// clang++ -dynamiclib $LDFLAGS -fopenmp main.cpp -lomp -std=c++11
 
 // using namespace std;
-using std::cout;
+using std::cout, std::endl;
 
 void create_single_pointer_2d_array(void) {
 
@@ -118,11 +123,89 @@ void create_array_of_pointers_3d_array(void) {
     delete[] A;
 }
 
-int main() {
+void matrix_multiplication(void) {
+
+    // matrix dimensions
+    const int X = 5;
+    const int Y = 7;
+    const int Z = 8;
+
+    // matrix declaration
+    int** A = new int*[X]; // size X x Y
+    int** B = new int*[Y]; // size Y x Z
+    int** C = new int*[X]; // size X x Z
+
+    // iteration indices
+    unsigned int i, j, k;
+
+    int result_slot;
+
+    // allocate memory
+    for (i = 0; i < X; i++) {
+        A[i] = new int[Y];
+
+        // result array
+        C[i] = new int[Z];
+    }
+
+    for (j = 0; j < Y; j++) {
+        B[j] = new int[Z];
+    }
+
+    // assign random values
+    srand(10);
+    for (i = 0; i < X; i++) {
+        for (j = 0; j < Y; j++) {
+            A[i][j] = rand() % 7;
+        }
+    }
+    for (i = 0; i < Y; i++) {
+        for (j = 0; j < Z; j++) {
+            B[i][j] = rand() % 7;
+        }
+    }
+
+    // perform matrix multiplication
+    for (i = 0; i < X; i++) {
+        for (j = 0; j < Z; j++) {
+            result_slot = 0;
+            for (k = 0; k < Y; k++) {
+                result_slot += A[i][k] * B[k][j];
+            }
+            C[i][j] = result_slot;
+        }
+    }
+
+    // print result
+    cout << "Result of multiplying A[X by Y] and B[Y by Z]: " << endl;
+    for (i = 0; i < X; i++) {
+        for (j = 0; j < Z; j++) {
+            cout << C[i][j] << " "; 
+        }
+        cout << "\n";
+    }
+
+    // deallocate memory
+    for (i = 0; i < X; i++) {
+        delete[] A[i];
+        delete[] C[i];
+    } 
+    delete[] A;
+    delete[] C;
+
+    for (j = 0; j < Y; j++) {
+        delete[] B[j];
+    }
+    delete[] B;
+
+}
+
+int main(void) {
 
     create_single_pointer_2d_array();
     create_array_of_pointers_2d_array();
     create_array_of_pointers_3d_array();
+    matrix_multiplication();
 
     return EXIT_SUCCESS;
 }
